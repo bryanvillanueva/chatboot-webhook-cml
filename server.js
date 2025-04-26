3f35ccfe0105c63b3b09ca7a9a02dac6c63a369b
Bryan Villanueva <vbryan0803@gmail.com>
2025-04-26 15:22:26 +1000

    prueba n8n

--- server.js
+++ server.js
@@ -88,7 +88,8 @@ app.post('/webhook', async (req, res) => {
 
       // Choose target webhook URL based on message type
       const webhookMap = {
-          'text': 'https://hook.eu2.make.com/qjqv5oykr1g4tt6qbyewwn293mu7oxob',
+          //'text': 'https://hook.eu2.make.com/qjqv5oykr1g4tt6qbyewwn293mu7oxob',
+          'text': 'http://localhost:5678/webhook/47bd0fdd-a8a6-44e1-a85d-c52340d578ca',
           'audio': 'https://hook.eu2.make.com/qjqv5oykr1g4tt6qbyewwn293mu7oxob',
           'image': 'https://hook.eu2.make.com/qjqv5oykr1g4tt6qbyewwn293mu7oxob',
           'document': 'https://hook.eu2.make.com/qjqv5oykr1g4tt6qbyewwn293mu7oxob'
